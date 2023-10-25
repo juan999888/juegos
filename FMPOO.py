b96eb978071458e46a72fc3308a3201a24536dff
BreynerAguilar <124077930+BreynerAguilar@users.noreply.github.com>
2023-10-25 08:14:45 -0500

    Update FMPOO.py

--- FMPOO.py
+++ FMPOO.py
@@ -14,8 +14,14 @@ class mindy_SC:
     def caer(self):
         self.mindy_y -= 10
 
+    def valvida(self):
+        return self.vida > 0
+
     def cvida(self):
         print("La vida de mindy es: ", self.vida)
+    
+    def morir(self):
+        self.vida = 0
 
 class boton_SC:
     def __init__(self, texture, valor, estado):
@@ -35,19 +41,28 @@ class pinchos_SC:
 
     def red_vida(self):
         mindy.vida = mindy.vida - self.daño
-        print("se le redujo", self.daño, "de vida a", mindy.nombre, "por caer en los pinchos")
+        if mindy.valvida():
+            print("se le redujo", self.daño, "de vida a", mindy.nombre, "por caer en los pinchos")
+        else:
+            mindy.morir()
+            print(mindy.nombre, "ha muerto al caer en los pinchos")
 
 class liquido_SC:
-    def __init__(self, nombre, color, dañol):
-        self.nombre = nombre
+    def __init__(self, nombrel, color, dañol):
+        self.nombrel = nombrel
         self.color = color
         self.dañol = dañol
     
     def red_vidal(self):
         mindy.vida = mindy.vida - self.dañol
-        print("se le redujo", self.dañol, "de vida a", mindy.nombre, "por caer en", self.nombre)
+        if mindy.valvida():
+            print("se le redujo", self.dañol, "de vida a", mindy.nombre, "por caer en", self.nombrel)
+        else:
+            mindy.morir()
+            print(mindy.nombre, "ha muerto por caer en", self.nombrel)
 
-mindy = mindy_SC("Mindy", 4, 240, 180)
+
+mindy = mindy_SC("Mindy", 3, 240, 180)
 pinchos = pinchos_SC(1, 1)
 pinchos.red_vida()
 mindy.cvida()
